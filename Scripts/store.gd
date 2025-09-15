82be645c50feacc7708601bdb1d1db7161c04e4a
NewPerspektive <balfedir2@gmail.com>
2025-09-15 17:11:29 +0200

    Added a working store and store UI

---
+++ Scripts/store.gd
@@ -0,0 +1,43 @@
+extends Node3D
+
+@export var brake_force: float = 100.0
+var car: VehicleBody3D
+var menu_opened := false
+
+func _ready():
+	var cars = get_tree().get_nodes_in_group("car")
+	if cars.size() > 0:
+		car = cars[0]
+	else:
+		push_error("No car found in group 'car'")
+
+
+func _on_trigger_area_body_entered(body: Node3D) -> void:
+	if body.is_in_group("car") and not menu_opened:
+		print("Car entered")
+		# Apply brakes
+		car.engine_force = 0
+		car.brake = brake_force
+
+		# Wait until the car slows down
+		await get_tree().create_timer(1.5).timeout
+		car.linear_velocity = Vector3.ZERO
+		car.engine_force = 0
+		car.brake = brake_force * 5
+		get_tree().paused = true
+		_open_store_menu()
+	
+func _open_store_menu():
+	print("Menu opened")
+	menu_opened = true
+
+	# Show store menu (use group instead of hard path)
+	var uis = get_tree().get_nodes_in_group("store_ui")
+	var ui = uis[0]
+	ui.visible = true
+
+	# Freeze the car fully
+	car.linear_velocity = Vector3.ZERO
+	car.engine_force = 0
+	car.brake = brake_force * 5
+	
