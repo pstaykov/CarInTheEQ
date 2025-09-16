3d067116349acd353350baafe2a001a11c5e77cc
NewPerspektive <balfedir2@gmail.com>
2025-09-16 16:00:31 +0200

    Quick fix the string formatting

--- Scripts/StoreMenu.gd
+++ Scripts/StoreMenu.gd
@@ -11,7 +11,8 @@ func _on_MainMenu_button_pressed():
 	get_tree().change_scene_to_file("res://Scenes/SongSelect.tscn")
 	
 func _process(delta: float) -> void:
-	$VBoxContainer/BuyRareSong.text = "Buy rare song " + Global.disc_count + "/15 " 
+	$VBoxContainer/BuyRareSong.text = "Buy rare song %d/15"%[Global.disc_count]
+	
 
 func _on_RareSongButton_pressed():
 	Global.disc_count -= 15
