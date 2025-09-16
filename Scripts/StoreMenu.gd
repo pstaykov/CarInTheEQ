78768a61ef967408a4cd09cac3a310430eacb382
NewPerspektive <balfedir2@gmail.com>
2025-09-16 19:34:00 +0200

    Fixed endless song aquiering bug

--- Scripts/StoreMenu.gd
+++ Scripts/StoreMenu.gd
@@ -5,6 +5,12 @@ func _ready() -> void:
 	$VBoxContainer/BuyRareSong.pressed.connect(_on_RareSongButton_pressed)
 	$VBoxContainer/UnlockSpace.pressed.connect(_on_UnlockSpaceButton_pressed)
 	add_to_group("store_ui")
+	update_labels()  # set initial text
+
+
+func update_labels() -> void:
+	$VBoxContainer/BuyRareSong.text = "Buy rare song %d/15" % Global.disc_count
+	$VBoxContainer/UnlockSpace.text = "Unlock space %d/5" % Global.casette_count
 
 
 func _on_MainMenu_button_pressed():
@@ -12,13 +18,18 @@ func _on_MainMenu_button_pressed():
 	get_tree().change_scene_to_file("res://Scenes/SongSelect.tscn")
 	
 func _process(delta: float) -> void:
-	$VBoxContainer/BuyRareSong.text = "Buy rare song %d/15"%[Global.disc_count]
-	$VBoxContainer/UnlockSpace.text = "Unlock space %d/5"%[Global.casette_count]
+	update_labels() 
+
 
 func _on_RareSongButton_pressed():
-	Global.disc_count -= 15
-	Global.unlock_song(Global.songs.keys().pick_random())
+	if Global.disc_count >= 15:
+		Global.disc_count -= 15
+		Global.unlock_song(Global.songs.keys().pick_random())
+		update_labels()  # refresh after purchase
+
 
 func _on_UnlockSpaceButton_pressed():
-	Global.casette_count -= 5
-	Global.SpaceUnlocked = true
+	if Global.casette_count >= 5:
+		Global.casette_count -= 5
+		Global.SpaceUnlocked = true
+		update_labels()  # refresh after purchase
